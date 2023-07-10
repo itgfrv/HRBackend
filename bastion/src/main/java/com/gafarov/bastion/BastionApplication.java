8cc5f62b49e05ee619f48ae24c3480646a86d293
Gafar0v <gafaroffis@yandex.ru>
2023-07-10 10:27:36 +0300

    init java project

---
+++ bastion/src/main/java/com/gafarov/bastion/BastionApplication.java
@@ -0,0 +1,13 @@
+package com.gafarov.bastion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BastionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BastionApplication.class, args);
+	}
+
+}
---
+++ bastion/src/test/java/com/gafarov/bastion/BastionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gafarov.bastion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BastionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
